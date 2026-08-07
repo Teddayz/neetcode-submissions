78d81384f1fd3aed1cdb1fefa26fabd21f99cb02
Mervyn Neo <130145360+Teddayz@users.noreply.github.com>
2026-08-08 00:31:41 +0800

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        fastest_rate = len(piles)
+        for pile in piles:
+            fastest_rate = max(fastest_rate, pile)
+        slowest_rate = 1
+        # Minimum banana per hour = k
+        # Fastest time = len(piles) -> This means that my k = maximum of array
+        # Slowest time -> k == 1 where i eat 1 banana per hour
+        while slowest_rate <= fastest_rate:
+            middle_rate = (slowest_rate + fastest_rate) // 2
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile / middle_rate)
+            if hours > h:
+                slowest_rate = middle_rate + 1
+            else:
+                fastest_rate = middle_rate - 1
+        return slowest_rate
